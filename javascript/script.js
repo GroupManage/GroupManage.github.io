6a5e59cd603245480eb48ce266d50d9977401829
Simon Vareille <si.vareille@gmail.com>
2017-02-23 19:32:28 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -50,3 +50,30 @@ function GetURLParameter(sParam)
 var tech = GetURLParameter('technology');
 var blog = GetURLParameter('blog');*/
 
+{
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.images;
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    for(var i=0; i<img.length; i++){
+        img[i].onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            modalImg.alt = this.alt;
+            modalImg.title = this.title;
+            captionText.innerHTML = this.alt;
+        }
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
